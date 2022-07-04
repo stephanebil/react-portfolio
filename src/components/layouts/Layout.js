11094c39c9944f0f3b340016e30b34c906dae783
stephanebil <stephanebilella@yahoo.fr>
2022-07-04 16:06:27 +0200

    portfolio summit 2

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -7,16 +7,32 @@ export default function Layout({children}) {
     <div>
       <Navigation />
       <main className="">
-        <div className="bloc-general-information">
+        <div className="bloc-general-information w-[320px] md:w-[640px]">
           <img
             src="img/xsmith.jpg.pagespeed.ic.-fvXWI23nq.webp"
             alt="type avec lunettes image principale"
           />
-          <div className="">
+          <div className="bg-bluegeneraleinformation">
             <h3 className="">Generale Information</h3>
             <div className="flex">
               <img src="img/téléchargement (4).webp" alt="icone crayon" />
               <p>Name: Justin Bieber</p>
+            </div>
+            <div className="flex">
+              <img src="img/téléchargement (1).webp" alt="icon localisation" />
+              <p>Location: London UK</p>
+            </div>
+            <div className="flex">
+              <img src="img/téléchargement (5).webp" alt="icone mail" />
+              <p>contactme@templatename.com</p>
+            </div>
+            <div className="flex">
+              <img src="img/téléchargement (2).webp" alt="icone phone" />
+              <p>+76 6524 567862 763</p>
+            </div>
+            <div className="flex">
+              <img src="img/téléchargement (3).webp" alt="icone phone" />
+              <p>mytemplatename.com</p>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
-	presets: [require("./tailwind-preset")],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  presets: [require("./tailwind-preset")],
+  theme: {
+    extend: {
+      colors: {
+        "bluegeneraleinformation": "#100f3a",
+        
+		
+		"violethomecard-educal": "#8007e6",
+        "bluehomecard-educal": "#2b4eff",
+        "bluehomecard-educal2": "#2d69f0",
+        "bluehomeletter-educal": "#0fa0dd",
+        "bluehomelight-educal": "#edeef3",
+        "bluenav1-educal": "#b8dde3",
+        "grayhomeletter-educal": "#6d6e75",
+        "graynav1-educal": "#6d6e75",
+        "orangehomeletter-educal": "#fa7919",
+        "greenhomecard-educal": "#30a820",
+        "yellow-educal": "#fc1",
+        "red-educal": "#f00",
+        "green-educal": "#0f0",
+      },
+    },
+  },
+  plugins: [],
 };
