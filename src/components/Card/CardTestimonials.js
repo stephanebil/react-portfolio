baebdd1bf83be756e633c4e669eac83ad003dcc0
stephanebil <stephanebilella@yahoo.fr>
2022-07-05 16:53:56 +0200

    portfolio Témoignagespage commencée 2

--- src/components/Card/CardTestimonials.js
+++ src/components/Card/CardTestimonials.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function CardTestimonials() {
   return (
-    <main>CardTestimonials</main>
+    <main>
+        <h3></h3>
+        <p></p>
+        <img src="" alt="" />
+        <p><span></span>Customer</p>
+    </main>
   )
 }
