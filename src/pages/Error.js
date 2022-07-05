c597d182cf272eac5c9b6db1701ec98802817503
stephanebil <stephanebilella@yahoo.fr>
2022-07-05 16:50:07 +0200

    portfolio Témoignagespage commencée

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,7 +3,11 @@ import Layout from '../components/layouts/Layout'
 
 export default function Error() {
   return (
-    <Layout><p>Error 404</p></Layout>
-    
-  )
+    <Layout>
+      <div className="text-center">
+        <h2 className="text-bluegeneraleinformation font-[700] font-[30px]">Error 404</h2>
+        <p>Désolé, page introuvable, veuillez renouveler votre recherche.</p>
+      </div>
+    </Layout>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from "../components/layouts/Layout";
+import AboutSection1 from "../components/AboutPage/AboutSection1";
 
 export default function Home() {
   return (
-    <Layout> 
-       <p>home</p> 
+    <Layout>
+      <AboutSection1 />
     </Layout>
-  )
+  );
 }
